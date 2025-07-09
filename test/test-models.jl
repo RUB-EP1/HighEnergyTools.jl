c58b8e373d9577c3df58a3f2425a6b33b323106f
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2025-07-09 22:57:04 +0200

    cherry pick fix from sWeights

--- test/test-models.jl
+++ test/test-models.jl
@@ -28,7 +28,7 @@ model = build_model(anka, pars)
 
 @testset "Anka model" begin
     @test model.components[1] isa Truncated{<:Normal}
-    @test model.components[2] isa LocationScale{A, B, <:Chebyshev} where {A, B}
+    @test model.components[2] isa LocationScale{A, B, <:DistributionsHEP.StandardChebyshev} where {A, B}
     @test model.prior.p[1] ≈ 0.5
     @test model.prior.p[2] ≈ 0.5
 end
@@ -57,6 +57,6 @@ model = build_model(frida, pars)
 @testset "Frida model" begin
     @test model.components[1] isa Truncated{<:Normal}
     @test model.components[2] isa Truncated{<:Normal}
-    @test model.components[3] isa LocationScale{A, B, <:Chebyshev} where {A, B}
+    @test model.components[3] isa LocationScale{A, B, <:DistributionsHEP.StandardChebyshev} where {A, B}
 end
 
